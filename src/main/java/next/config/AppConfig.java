e12e5a75097030c9171aecbf4f4149eb8e2426cf
viz <yeechan.ko@nhnnext.org>
2017-06-09 11:16:29 +0900

    4번

--- src/main/java/next/config/AppConfig.java
+++ src/main/java/next/config/AppConfig.java
@@ -1,8 +1,9 @@
 package next.config;
 
+import java.beans.PropertyVetoException;
+
 import javax.sql.DataSource;
 
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -16,59 +17,59 @@ import org.springframework.context.support.ReloadableResourceBundleMessageSource
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 import next.support.DBInitializer;
 
 @Configuration
-@ComponentScan(
-	basePackages = { "next.controller", "next.service", "next.dao" },
-	excludeFilters = @ComponentScan.Filter(value = Controller.class, type = FilterType.ANNOTATION)
-)
+@ComponentScan(basePackages = { "next.controller", "next.service",
+		"next.dao" }, excludeFilters = @ComponentScan.Filter(value = Controller.class, type = FilterType.ANNOTATION))
 @PropertySource("classpath:application.properties")
-@EnableAspectJAutoProxy(proxyTargetClass=true)
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class AppConfig {
 	@Value("${db.driver.class}")
 	private String driverClass;
-	
+
 	@Value("${db.url}")
 	private String url;
-	
+
 	@Value("${db.username}")
 	private String username;
-	
+
 	@Value("${db.password}")
 	private String password;
-	
+
 	@Bean
-	public DataSource dataSource() {
-		BasicDataSource ds = new BasicDataSource();
-		ds.setDriverClassName(driverClass);
-		ds.setUrl(url);
-		ds.setUsername(username);
-		ds.setPassword(password);
-		return ds;
+	public DataSource dataSource() throws PropertyVetoException {
+		ComboPooledDataSource cpds = new ComboPooledDataSource();
+		cpds.setDriverClass(driverClass);
+		cpds.setJdbcUrl(url);
+		cpds.setUser(username);
+		cpds.setPassword(password);
+		return cpds;
 	}
-	
+
 	@Bean
 	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
 		return new JdbcTemplate(dataSource);
 	}
-	
+
 	@Bean
-    public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
-        return new PropertySourcesPlaceholderConfigurer();
-    }
-	
+	public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+
 	@Bean
 	public MessageSource messageSource() {
-	    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-	    messageSource.setBasename("classpath:messages");
-	    messageSource.setDefaultEncoding("UTF-8");
-	    messageSource.setCacheSeconds(30);
-	    return messageSource;
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		messageSource.setCacheSeconds(30);
+		return messageSource;
 	}
-	
+
 	@Bean
-	public DBInitializer dbInitializer(){
+	public DBInitializer dbInitializer() {
 		return new DBInitializer();
 	}
 }
